68f4eee9aa6c6399e6dfb507d0aa76356d34ba7e
Ohad Yarel <ohadyarel@gmail.com>
2016-05-03 18:49:39 -0400

    creating sign-up, log-in and log-out functionalities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   def create
+  	user = User.where(email: params[:session][:email].downcase).first
+    if user && user[:password] == params[:session][:password]
+    	log_in user
+    	redirect_to user_path(user)
+    else
+	  	flash[:alert] = "Wrong email or password. please try again."
+	  	render 'new'
+	  end
   end
 
   def new
   end
 
   def destroy
+  	log_out if logged_in?
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,24 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    if User.where(email: params[:user][:email].downcase) == nil
+      @user = User.new(blog_title: params[:user][:blog_title], name: params[:user][:name], email: params[:user][:email].downcase, password: params[:user][:password])
+      @user.blog_cat = "General"
+      @user.save
+      log_in @user
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "A user with that email already exists"
+      redirect_to new_user_path
+    end
   end
 
   def edit
