86cff55512b14df36ee904808c8d5dec8d16fbcb
Ohad Yarel <ohadyarel@gmail.com>
2016-05-03 19:00:10 -0400

    adding new-post and edit-profile buttons on user show.html.erb if the user is the user in session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-	def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   def logged_in?
     !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def log_out
