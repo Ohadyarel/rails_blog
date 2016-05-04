c43f88838ece5799e5dffd9b712962a5dda25012
Ohad Yarel <ohadyarel@gmail.com>
2016-05-04 16:16:04 -0400

    adding form to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    if !params[:post][:title].blank? && !params[:post][:desc].blank? && !params[:post][:body].blank? && logged_in?
+      @post = Post.create(title: params[:post][:title], desc: params[:post][:desc], body: params[:post][:body], user_id: current_user.id)
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Please fill in all the fields above."
+      redirect_to new_post_path
+    end
   end
 
   def destroy
